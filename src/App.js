c2efe34e5ac4aa4ad4d7cf6d51dac5b9ae084aa4
AhsanulHaq777 <66363908+AhsanulHaq777@users.noreply.github.com>
2020-06-09 14:26:56 +0500

    Updated App.js file Text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Covid19 Tracking APP.</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          COMING SOON!
         </p>
         <a
           className="App-link"
